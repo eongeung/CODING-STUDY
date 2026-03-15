56680b7cf41cb1cc992851e1fdaef079dd472f0b
Yeong Heun Lee <151762285+eongeung@users.noreply.github.com>
2026-03-15 18:18:21 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,9 +1,5 @@
 n = int(input())
-numbers = list(map(int,input().split()))
+numbers = list(map(int, input().split()))
 v = int(input())
-count = 0
 
-for i in range(n):
-    if v == numbers[i]:
-        count += 1
-print(count)
+print(numbers.count(v))
