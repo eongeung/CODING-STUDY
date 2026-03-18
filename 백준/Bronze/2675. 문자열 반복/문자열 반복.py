80da754025053f55d0b79dac0591e265cd4c16da
Yeong Heun Lee <151762285+eongeung@users.noreply.github.com>
2026-03-19 00:20:56 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,8 +1,4 @@
 t = int(input())
 for i in range(t):
     r,s = input().split()
-    r = int(r)
-    result = ""
-    for c in s:
-        result += c * r
-    print(result)
+    print("".join(c * int(r) for c in s))
